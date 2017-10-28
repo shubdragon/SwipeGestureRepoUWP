6b88337ccde6ff8b5181cb2c89b5328142938027
shubdragon <shubham.sahu@hotmail.com>
2017-10-28 12:05:04 +0530

    Show Swipe gesture on UWP

---
+++ SwipeGestureRepo/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace SwipeGestureRepo
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private bool _isSwiped;
+        private void SwipeableTextBlock_ManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e)
+        {
+            if (e.IsInertial && !_isSwiped)
+            {
+                var swipedDistance = e.Cumulative.Translation.X;
+
+                if (Math.Abs(swipedDistance) <= 2) return;
+
+                if (swipedDistance > 0)
+                {
+                    SwipeableTextBlock.Text = "Right Swiped";
+                }
+                else
+                {
+                    SwipeableTextBlock.Text = "Left Swiped";
+                }
+                _isSwiped = true;
+            }            
+        }
+
+        private void SwipeableTextBlock_ManipulationCompleted(object sender, ManipulationCompletedRoutedEventArgs e)
+        {
+            _isSwiped = false;
+        }        
+    }
+}
